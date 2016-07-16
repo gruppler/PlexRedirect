b8de2a934d181a43f19fc9161bb016701e08afa3
Craig Laparo <gruppler+github@gmail.com>
2016-07-16 14:47:25 -0400

    Describe config variables

--- config.default.php
+++ config.default.php
@@ -1,11 +1,22 @@
 <?php
 
+// The displayed name of the server
 $SERVER_NAME = $_SERVER['SERVER_NAME'];
+
+// The actual URL of the server
 $SERVER_URL = $_SERVER['SERVER_NAME'];
+
+// The URL to ping
 $PLEX_SERVER = $SERVER_URL.':32400';
-$PLEX_REQUESTS = $SERVER_URL.':3000';
+
+// PlexRequests URL
+$PLEX_REQUESTS = $SERVER_URL.':3000/search';
+
+// PlexEmail URL
 $PLEX_EMAIL = '';
 
-$PLEX_URL = 'app.plex.tv/web/app';
+// Plex App URL
+// $PLEX_URL = 'app.plex.tv/web/app';
+$PLEX_URL = $PLEX_SERVER.'/web';
 
 ?>
